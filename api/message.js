58deb6dd06625ff61b496a9f7c1f755ae31dfc02
omae-muds <57761233+omae-muds@users.noreply.github.com>
2021-10-20 19:21:59 +0900

    ready1

---
+++ api/message.js
@@ -0,0 +1,24 @@
+const Pusher = require('pusher');
+
+require('dotenv').config();
+const env = process.env
+
+const pusher = new Pusher({
+  appId: env.PUSHER_APP_ID,
+  key: env.PUSHER_KEY,
+  secret: env.PUSHER_SECRET,
+  cluster: env.PUSHER_CLUSTER,
+  useTLS: true
+});
+
+module.exports = (req, res) => {
+  const body = JSON.parse(req.body);
+  const pusherResp = {
+    username: body.username,
+    message: body.message
+  };
+
+  pusher.trigger(env.PUSHER_CHAT_CHANNEL, env.PUSHER_MESSAGE_EVENT, pusherResp)
+    .then(resp => res.status(200).send(resp))
+    .catch(err => res.status(400).send(err));
+};
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,59 @@
 <script>
-	export let name;
+	import {onMount} from 'svelte';
+    import Pusher from 'pusher-js';
+	
+	require('dotenv').config();
+	const env = process.env
+
+    let username = 'username';
+    let message = '';
+    let messages = [];
+    onMount(() => {
+        Pusher.logToConsole = true;
+        const pusher = new Pusher(env.PUSHER_KEY, {
+            cluster: env.PUSHER_CLUSTER
+        });
+        const channel = pusher.subscribe(env.PUSHER_CHAT_CHANNEL);
+        channel.bind(env.PUSHER_MESSAGE_EVENT, data => {
+            messages = [...messages, data];
+        });
+    })
+    const submit = async () => {
+        await fetch('http://localhost:8000/api/message', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                username,
+                message
+            })
+        });
+        message = '';
+    };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="container">
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+        <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+            <input class="fs-5 fw-semibold" bind:value={username}/>
+        </div>
+        <div class="list-group list-group-flush border-bottom scrollarea">
+            {#each messages as msg}
+                <div class="list-group-item list-group-item-action py-3 lh-tight">
+                    <div class="d-flex w-100 align-items-center justify-content-between">
+                        <strong class="mb-1">{msg.username}</strong>
+                    </div>
+                    <div class="col-10 mb-1 small">{msg.message}</div>
+                </div>
+            {/each}
+        </div>
+    </div>
+    <form on:submit|preventDefault={submit}>
+        <input class="form-control" placeholder="Write a message" bind:value={message}/>
+    </form>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    .scrollarea {
+        min-height: 500px;
+    }
 </style>
