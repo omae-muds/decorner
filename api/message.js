5c9f479f517fba088f383f22165f6ce630c5bc8e
omae-muds <57761233+omae-muds@users.noreply.github.com>
2021-10-21 13:50:08 +0900

    Fix: dotenv, JSONParse

--- api/message.js
+++ api/message.js
@@ -1,21 +1,24 @@
 const Pusher = require('pusher');
 
+require('dotenv').config();
+const env = process.env;
+
 const pusher = new Pusher({
-  appId: PUSHER_APP_ID,
-  key: PUSHER_KEY,
-  secret: PUSHER_SECRET,
-  cluster: PUSHER_CLUSTER,
+  appId: env.PUSHER_APP_ID,
+  key: env.PUSHER_KEY,
+  secret: env.PUSHER_SECRET,
+  cluster: env.PUSHER_CLUSTER,
   useTLS: true
 });
 
 module.exports = (req, res) => {
-  const body = JSON.parse(req.body);
+  const body = req.body;
   const pusherResp = {
     username: body.username,
     message: body.message
   };
 
-  pusher.trigger(PUSHER_CHAT_CHANNEL, PUSHER_MESSAGE_EVENT, pusherResp)
+  pusher.trigger(env.PUSHER_CHAT_CHANNEL, env.PUSHER_MESSAGE_EVENT, pusherResp)
     .then(resp => res.status(200).send(resp))
     .catch(err => res.status(400).send(err));
 };
