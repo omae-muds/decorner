721a197e976e200c2b4c002977b5a7c782cfe6a2
Hayashikotarou <s1922028@stu.musashino-u.ac.jp>
2021-10-21 21:46:07 +0900

    test-message-area

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
     import {onMount} from 'svelte';
     import Pusher from 'pusher-js';
+import { get_all_dirty_from_scope } from 'svelte/internal';
 
     let username = 'username';
     let message = '';
@@ -37,20 +38,70 @@
             {#each messages as msg}
                 <div class="list-group-item list-group-item-action py-3 lh-tight">
                     <div class="d-flex w-100 align-items-center justify-content-between">
+
                         <strong class="mb-1">{msg.username}</strong>
                     </div>
+                    
                     <div class="col-10 mb-1 small">{msg.message}</div>
                 </div>
             {/each}
         </div>
     </div>
-    <form on:submit|preventDefault={submit}>
-        <input class="form-control" placeholder="Write a message" bind:value={message}/>
-    </form>
+    <div class="message-area">
+        <div class="message-area-text">
+            <form on:submit|preventDefault={submit}>
+                <input id="text-area" placeholder="Write a message" bind:value={message}/>
+            </form>
+        </div>
+        <div class="message-area-send">
+            <form>
+                <input id="send-area" type="submit" value="Send">
+            </form>
+        </div>
+    </div>
 </div>
 
 <style>
+
+    .message-area {
+        display: flex;
+}
+    .message-area-text {
+        width: 90%;
+}
+    .message-area-send {
+        width: 10%;
+}
+
+    #send-area {
+        width: 100%;
+        height: 100%;
+        background:greenyellow;
+        font-size: 1.0rem;
+        font-weight: bold;
+        /* padding: 0.5em 1em; */
+        /* margin: 2em 0; */
+        border: solid 3px #6091d3;/*線*/
+        border-radius: 6px;/*角の丸み*/
+        box-sizing: border-box;
+}
+    #text-area {
+        width: 100%;
+        height: 100%;
+        background: ghostwhite;
+        font-size: 1.0rem;
+        font-weight: bold;
+        /* padding: 0.5em 1em; */
+        /* margin: 2em 0; */
+        border: solid 3px #6091d3;/*線*/
+        border-radius: 6px;/*角の丸み*/
+        box-sizing: border-box;
+}
+
+
     .scrollarea {
+        overflow: auto; /* スクロールを効かせつつ、メッセージがタイムラインの外に出ないようにする */
         min-height: 500px;
+        /* height:100%;テキストエリアが下に張り付く様にする */
     }
 </style>
