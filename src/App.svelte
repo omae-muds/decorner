dba406cfc1868b31f3d7fdf14e694973c1de9592
Hayashikotarou <s1922028@stu.musashino-u.ac.jp>
2021-10-22 01:05:49 +0900

    test/send

--- src/App.svelte
+++ src/App.svelte
@@ -28,56 +28,96 @@ import { get_all_dirty_from_scope } from 'svelte/internal';
         message = '';
     };
 </script>
+<div class="body">
+    <div class="container">
+        <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+            <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+                <input class="fs-5 fw-semibold" id ='user-area' bind:value={username}/>
+            </div>
+            <div class="list-group list-group-flush border-bottom scrollarea">
+                {#each messages as msg}
+                    <div class="list-group-item list-group-item-action py-3 lh-tight">
+                        <div class="d-flex w-100 align-items-center justify-content-between">
 
-<div class="container">
-    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
-        <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-            <input class="fs-5 fw-semibold" bind:value={username}/>
-        </div>
-        <div class="list-group list-group-flush border-bottom scrollarea">
-            {#each messages as msg}
-                <div class="list-group-item list-group-item-action py-3 lh-tight">
-                    <div class="d-flex w-100 align-items-center justify-content-between">
-
-                        <strong class="mb-1">{msg.username}</strong>
+                            <strong class="mb-1">{msg.username}</strong>
+                        </div>
+                        
+                        <div class="col-10 mb-1 small">{msg.message}</div>
                     </div>
-                    
-                    <div class="col-10 mb-1 small">{msg.message}</div>
-                </div>
-            {/each}
+                {/each}
+            </div>
         </div>
-    </div>
-    <div class="message-area">
-        <div class="message-area-text">
-            <form on:submit|preventDefault={submit}>
-                <input id="text-area" placeholder="Write a message" bind:value={message}/>
-            </form>
-        </div>
-        <div class="message-area-send">
-            <form>
-                <input id="send-area" type="submit" value="Send">
-            </form>
+        <div class="message-area">
+            <div class="message-area-text">
+                <form on:submit|preventDefault={submit}>
+                    <input id="text-area" placeholder="Write a message" bind:value={message}/>
+                </form>
+            </div>
+            <div class="message-area-send">
+                <form>
+                    <input id="send-area" type="submit" value="Send">
+                </form>
+            </div>
         </div>
     </div>
 </div>
 
 <style>
+    .body {
+        height: 100%;
+}
+    #user-area{
+        border-radius: 6px;/*角の丸み*/
+        position: fixed;
+    }
+    .container {
+        background-color:rgb(100,150,195);
+        height: 100%;
+        width: 100%;
+        /* left: 0;
+        right: 0; */
+    }
+
+    .scrollarea {
+        background-color:rgb(100,150,195);
+        overflow-y: scroll; /* スクロールを効かせつつ、メッセージがタイムラインの外に出ないようにする */
+        min-height: 100%;
+    }
 
     .message-area {
+        width: 100%;
         display: flex;
+        bottom: 0; /*下に固定*/
+        /* padding-bottom: 0%; */
+        /* position: absolute; */
+        position: fixed;
+        max-height: 100vh;
+        /* right: auto;
+        left: auto; */
 }
     .message-area-text {
         width: 90%;
+        word-wrap: break-word;
+        /* position: fixed; */
+        /* position: absolute; */
+        /* bottom: 0; 下に固定 */
+        /* overflow-y: scroll;*/
+        max-height: 100vh;
+        /* right: auto; */
 }
     .message-area-send {
         width: 10%;
+        margin-right: 1em;
+        /* position: fixed; */
+        /* position: absolute; */
+        /* bottom: 0; 下に固定 */
+        /* right: auto; */
 }
-
     #send-area {
         width: 100%;
         height: 100%;
         background:greenyellow;
-        font-size: 1.0rem;
+        /* font-size: 1.0rem; */
         font-weight: bold;
         /* padding: 0.5em 1em; */
         /* margin: 2em 0; */
@@ -89,7 +129,7 @@ import { get_all_dirty_from_scope } from 'svelte/internal';
         width: 100%;
         height: 100%;
         background: ghostwhite;
-        font-size: 1.0rem;
+        /* font-size: 1.0rem; */
         font-weight: bold;
         /* padding: 0.5em 1em; */
         /* margin: 2em 0; */
@@ -97,11 +137,4 @@ import { get_all_dirty_from_scope } from 'svelte/internal';
         border-radius: 6px;/*角の丸み*/
         box-sizing: border-box;
 }
-
-
-    .scrollarea {
-        overflow: auto; /* スクロールを効かせつつ、メッセージがタイムラインの外に出ないようにする */
-        min-height: 500px;
-        /* height:100%;テキストエリアが下に張り付く様にする */
-    }
 </style>
