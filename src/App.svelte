a9b84d748edbc52d1576847dfe6f3b664bdb8dd9
Hayashikotarou <s1922028@stu.musashino-u.ac.jp>
2021-10-22 04:03:27 +0900

    test

--- src/App.svelte
+++ src/App.svelte
@@ -57,12 +57,8 @@ import { get_all_dirty_from_scope } from 'svelte/internal';
                 <form on:submit|preventDefault={submit}>
                     <input id="text-area" placeholder="Write a message" bind:value={message}/>
                 </form>
-            <!-- </div>
-            <div class="message-area-send">
-                <form>
-                    <input id="send-area" type="submit" bind:value={message}/>
-                </form>
-            </div> -->
+
+                
         </div>
     </div>
 </div>
