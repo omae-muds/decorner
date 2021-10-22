8103ed1b0d707d417427c0ff9f927631ca3d31ce
Hayashikotarou <s1922028@stu.musashino-u.ac.jp>
2021-10-22 14:36:15 +0900

    send

--- src/App.svelte
+++ src/App.svelte
@@ -60,7 +60,7 @@ import { get_all_dirty_from_scope } from 'svelte/internal';
             </div>
             <div class="message-area-send">
                 <form on:submit|preventDefault={submit}>
-                    <input id="send-area" type="submit" formmethod="POST" value="送信">
+                    <input id="send-area" type="submit" formmethod="POST" value="Send">
                 </form>
             </div>
 
