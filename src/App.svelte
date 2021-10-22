c725edae472ad27a3f2bdeeaebcb84c11543fa8e
Hayashikotarou <s1922028@stu.musashino-u.ac.jp>
2021-10-22 13:22:56 +0900

    test

--- src/App.svelte
+++ src/App.svelte
@@ -58,11 +58,11 @@ import { get_all_dirty_from_scope } from 'svelte/internal';
                     <input id="text-area" placeholder="Write a message" bind:value={message}/>
                 </form>
             </div>
-            <!-- <div class="message-area-send">
+            <div class="message-area-send">
                 <form>
                     <input id="send-area" type="submit" bind:value={message}/>
                 </form>
-            </div> -->
+            </div>
 
 
         </div>
@@ -101,7 +101,7 @@ import { get_all_dirty_from_scope } from 'svelte/internal';
     }
 
     .scrollarea {
-        position:relative;
+        position:absolute;
         background-color:rgb(100,150,195);
         z-index: -1;
         overflow-y: scroll; /* スクロールを効かせつつ、メッセージがタイムラインの外に出ないようにする */
