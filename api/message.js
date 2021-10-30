86727726a523517728ec03f6d29e633ad5d79649
omae-muds <57761233+omae-muds@users.noreply.github.com>
2021-10-31 01:47:38 +0900

    Add apologize logic

--- api/message.js
+++ api/message.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const Pusher = require('pusher');
 
 require('dotenv').config();
@@ -11,15 +12,42 @@ const pusher = new Pusher({
   useTLS: true,
 });
 
+
+fs.readFile('banwords.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  const banwords = JSON.parse(data);
+})
+fs.readFile('apologiesList.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  const apologiesList = JSON.parse(data);
+})
+
+
 module.exports = (req, res) => {
   const body = req.body;
-  const pusherResp = {
+  const pushMessage = {
     userId: body.userId,
     username: body.username,
     message: body.message,
   };
 
-  pusher.trigger(env.PUSHER_CHAT_CHANNEL, env.PUSHER_MESSAGE_EVENT, pusherResp)
+  pusher.trigger(env.PUSHER_CHAT_CHANNEL, env.PUSHER_MESSAGE_EVENT, pushMessage)
     .then(resp => res.status(200).send(resp))
     .catch(err => res.status(400).send(err));
+
+  if (body.message in banwords) {
+    apologiesList.forEach(apologies => {
+      apologies.forEach(apology => {
+        const pushApology = {
+          userId: body.userId,
+          username: body.username,
+          message: apology,
+        };
+
+        pusher.trigger(env.PUSHER_CHAT_CHANNEL, env.PUSHER_MESSAGE_EVENT, pushApology)
+          .then(resp => res.status(200).send(resp))
+          .catch(err => res.status(400).send(err));
+      });
+    });
+  }
 };
